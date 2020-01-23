cd9bcffe1dca6abbf7d1a9896f2347de2e851411
mariiashybetska <dp190594smo@gmail.com>
2020-01-23 21:21:55 +0200

    xdgd

--- src/students/admin.py
+++ src/students/admin.py
@@ -4,9 +4,16 @@ from students.models import Student
 
 
 class StudentAdmin(admin.ModelAdmin):
-    readonly_fields = ('email', 'telephone')
+    # readonly_fields = ('email', 'telephone')
     list_display = ('id', 'first_name', 'last_name', 'group_id')
     list_select_related = ('group_id',)
+    list_per_page = 10
+
+    def get_readonly_fields(self, request, obj=None):
+        if request.user.groups.filter(name='manager').exists():
+            return ('email', 'telephone')
+        return ()
+
 
 
 admin.site.register(Student, StudentAdmin)
