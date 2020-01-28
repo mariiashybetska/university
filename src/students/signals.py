0f38e9fea946afeac9959223676440453f21a02f
mariiashybetska <dp190594smo@gmail.com>
2020-01-28 20:41:36 +0200

    start HW_l13

--- src/students/signals.py
+++ src/students/signals.py
@@ -3,10 +3,15 @@ from django.dispatch import receiver
 
 from students.models import Student
 
+import string
+
 
 @receiver(pre_save, sender=Student)
 def pre_save_student(sender, instance, **kwargs):
     instance.email = instance.email.lower()
+    instance.first_name = string.capwords(instance.first_name)
+    instance.last_name = string.capwords(instance.last_name)
+    instance.telephone = ''.join(x for x in instance.telephone if x.isdigit())
 
     if instance.id is None:
         print('Object is created!')
