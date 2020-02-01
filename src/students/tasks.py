899719396662f911b1d80c10c8b93ae2055cee20
mariiashybetska <dp190594smo@gmail.com>
2020-02-01 19:03:31 +0200

    add registration form and e-mail confirmation

--- src/students/tasks.py
+++ src/students/tasks.py
@@ -3,6 +3,7 @@ from celery import shared_task
 from time import sleep
 
 from django.core.mail import send_mail
+from django.conf import settings
 
 
 
@@ -16,8 +17,9 @@ def add(a, b):
 
 @shared_task
 def send_email_async(subject, message,
-                     email_from, recipient_list):
+                     email_from):
 
+    recipient_list = [settings.EMAIL_HOST_USER]
     send_mail(subject, message,
               email_from, recipient_list,
               fail_silently=False)
