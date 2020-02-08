165ccc1e9a05fa21d9544c6863e6612feb9d27bc
mariiashybetska <dp190594smo@gmail.com>
2020-02-09 01:24:21 +0200

    add celerybeat, create task for cleaning logs

--- src/students/tasks.py
+++ src/students/tasks.py
@@ -1,9 +1,11 @@
 from celery import shared_task
-
 from time import sleep
+from datetime import timedelta, datetime
 
 from django.core.mail import send_mail
 from django.conf import settings
+
+from students.models import Logger
 
 
 
@@ -23,4 +25,16 @@ def send_email_async(subject, message,
     send_mail(subject, message,
               email_from, recipient_list,
               fail_silently=False)
+
+
+# @shared_task
+# def delete_logs_test():
+#     Logger.objects.all().delete()
+
+
+@shared_task
+def delete_logs():
+    td = timedelta(days=-7)
+    Logger.objects.filter(created__lte=datetime.now() + td).delete()
+
 
--- src/students_tracker/settings.py
+++ src/students_tracker/settings.py
@@ -11,10 +11,10 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+from celery.schedules import crontab
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
@@ -27,7 +27,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*', '127.0.0.1']
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -37,6 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_celery_beat',
 
     'debug_toolbar',
     'silk',
@@ -80,7 +80,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'students_tracker.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
@@ -90,7 +89,6 @@ WSGI_APPLICATION = 'students_tracker.wsgi.application'
 #         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 #     }
 # }
-
 
 
 # Password validation
@@ -111,7 +109,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
@@ -124,7 +121,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
@@ -161,3 +157,14 @@ try:
     from students_tracker.settings_local import *
 except ImportError:
     print('settings_local module not found')
+
+CELERY_BEAT_SCHEDULE = {
+    # 'test_log_delete': {
+    #     'task': 'students.tasks.delete_logs_test',
+    #     'schedule': crontab(minute="*/1"),  # every minute
+    #     },
+    'log_delete': {
+        'task': 'students.tasks.delete_logs',
+        'schedule': crontab(minute=0, hour=0),  # everyday at midnight
+    },
+}
