f71a471fe2ed04bd0cd8b7490984564c85435599
mariiashybetska <dp190594smo@gmail.com>
2020-02-15 18:38:57 +0200

    deleted pid file from git, fixed syntax acording to flake8

--- src/students/views.py
+++ src/students/views.py
@@ -203,11 +203,15 @@ def custom_login(request):
 
 
 def handler404(request):
-    return render(request, '404.html', status=404)
+    return render(request,
+                  '404.html',
+                  status=404)
 
 
 def handler500(request):
-    return render(request, '500.html', status=500)
+    return render(request,
+                  '500.html',
+                  status=500)
 
 
 
